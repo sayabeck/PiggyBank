e2d553bf5b582eca8940bd6eb4216209cf56329f
sayabeck <sayabeck@mail.ru>
2022-03-19 18:10:57 +0600

    Initial Commit

---
+++ PiggyBank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PiggyBank
+//
+//  Created by mac mini on 3/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
